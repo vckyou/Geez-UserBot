dd624707ee05d7d0a4e6c72e7f5e24021c63a6b7
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-03 17:36:59 +0700

    Update welcomes.py

--- userbot/modules/welcomes.py
+++ userbot/modules/welcomes.py
@@ -115,7 +115,7 @@ async def save_welcome(event):
             await event.client.send_message(
                 BOTLOG_CHATID, f"#WELCOME \nID GRUP: {event.chat_id}"
                 "\nLord Memasang Pesan Perintah Welcome Digrup, Ini Adalah Catatan Pesan Welcome "
-                "Mohon Jangan Dihapus Lord!"
+                "Mohon Jangan Dihapus!"
             )
             msg_o = await event.client.forward_messages(
                 entity=BOTLOG_CHATID,
@@ -130,7 +130,7 @@ async def save_welcome(event):
     elif event.reply_to_msg_id and not string:
         rep_msg = await event.get_reply_message()
         string = rep_msg.text
-    success = "`Berhasil Menyimpan Pesan Welcome {} ツ`"
+    success = "`Berhasil Menyimpan Pesan Welcome {}`"
     if add_welcome_setting(event.chat_id, 0, string, msg_id) is True:
         await event.edit(success.format('Disini'))
     else:
@@ -165,9 +165,9 @@ async def del_welcome(event):
     except AttributeError:
         return await event.edit("`Running on Non-SQL mode!`")
     if rm_welcome_setting(event.chat_id) is True:
-        await event.edit("`Menghapus Pesan Welcome Berhasil Dilakukan ツ`")
+        await event.edit("`Menghapus Pesan Welcome Berhasil Dilakukan`")
     else:
-        await event.edit("`Anda Tidak Menyimpan Pesan Welcome Apapun Disini Lord ツ`")
+        await event.edit("`Anda Tidak Menyimpan Pesan Welcome Apapun Disini`")
 
 
 CMD_HELP.update({
