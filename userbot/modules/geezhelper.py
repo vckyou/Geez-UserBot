04a4a6f11f362766b32a638fb48b43dec4fb151e
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-04 05:23:39 +0700

    Update geezhelper.py

--- userbot/modules/geezhelper.py
+++ userbot/modules/geezhelper.py
@@ -21,7 +21,7 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/KENZO-404/Lynx-Userbot/Lynx-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/vckyou/Geez-UserBot/Geez-UserBot/varshelper.txt)")
 
 
 CMD_HELP.update({
