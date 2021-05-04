b905c2654a4a3efd893c7254258c0d9c81c28d62
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-04 13:17:39 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-# You can find misc modules, which dont fit in anything xD
-""" Userbot module for other small commands. """
-
 from random import randint
 from time import sleep
 from os import execl
