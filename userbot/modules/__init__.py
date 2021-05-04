c694e3850bdf70894300900f9d4569e24507a7d3
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-04 13:43:39 +0700

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
 """ Init file which loads all of the modules """
 from userbot import LOGS
 
