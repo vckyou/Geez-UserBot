f5c56cfda70892497d3e0796f43bc7961fe603c6
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-04 13:24:51 +0700

    Update CI_Test_Script.sh

--- init/CI_Test_Script.sh
+++ init/CI_Test_Script.sh
@@ -13,7 +13,7 @@ export SEMAPHORE_PROJECT_DIR=$(pwd)
 . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
 TELEGRAM_TOKEN=${BOT_API_KEY}
 export BOT_API_KEY TELEGRAM_TOKEN
-tg_sendinfo "<code>I am gonna merge staging into Lynx-Userbot</code>"
+tg_sendinfo "<code>I am gonna merge staging into Geez-UserBot</code>"
 cd
 git clone https://github.com/mkaraniya/OpenUserBot.git
 cd OpenUserBot
@@ -21,5 +21,5 @@ git remote set-url origin https://${GH_USERNAME}:${GH_PERSONAL_TOKEN}@github.com
 git fetch
 git checkout staging
 git pull origin staging
-git push --force origin staging:Lynx-Userbot
-tg_sendinfo "<code>I have merged all commits from staging into Lynx-Userbot</code>"
+git push --force origin staging: Geez-UserBot
+tg_sendinfo "<code>I have merged all commits from staging into Geez-UserBot</code>"
