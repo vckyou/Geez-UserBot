9ea8e64a1934372e971dc8db6dcfe39a35c393aa
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-04 13:32:12 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -51,7 +51,7 @@ createuser botuser
 
 cd ..
 echo "pg_ctl -D $PREFIX/var/lib/postgresql start" > startbot.sh
-echo "cd Lynx-Userbot" >> startbot.sh
+echo "cd Geez-UserBot" >> startbot.sh
 echo "python3 -m userbot" >> startbot.sh
 chmod 755 startbot.sh
 
