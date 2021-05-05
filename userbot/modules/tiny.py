0618fa307fe6068f9981708f546465ad74dc977f
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-05 09:09:15 +0700

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -23,7 +23,7 @@ async def _(event):
         return
     xx = await event.edit("`Memproses Tiny....`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("LordUserbot/lorduserbot.png")
+    im1 = Image.open("geez/geez.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
