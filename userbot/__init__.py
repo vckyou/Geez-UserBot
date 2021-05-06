59e59e0a3012f5023a510bd613212498830f1068
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-06 23:03:46 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -195,7 +195,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "Geez-UserBot")
 
 # Bot Version
-BOT_VER = os.environ.get("BOT_VER", "0.2.6")
+BOT_VER = os.environ.get("BOT_VER", "5.0")
 
 # Default .alive Username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
