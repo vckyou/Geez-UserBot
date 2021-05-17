5fea7e62879cf3196317663a99c9bc2fb91ccfec
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-17 15:18:41 +0700

    Update globalkick.py

--- userbot/modules/globalkick.py
+++ userbot/modules/globalkick.py
@@ -89,7 +89,7 @@ async def gspide(rk):
         return await rkp.edit(f"`{ALIVE_NAME}:`**Kesalahan! Pengguna tidak dikenal.**")
     if user:
         if user.id == 1282429349:
-            return await rkp.edit(f"`{ALIVE_NAME}`, _Anda Tidak Bisa Global Kick Kepada Pembuat Saya__")
+            return await rkp.edit(f"`{ALIVE_NAME}`, __Anda Tidak Bisa Global Kick Kepada Pembuat Saya__")
         try:
             await rk.client(BlockRequest(user))
             await rk.client(UnblockRequest(user))
