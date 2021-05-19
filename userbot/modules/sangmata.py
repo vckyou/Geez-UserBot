aff5330aaf5ac546a8db17f591ded35a659ec91c
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-19 23:47:12 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -16,36 +16,19 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Reply Ke Pesan Pengguna Yang Ingin Anda Scan Yang Mulia.```")
+        await steal.edit("```Mohon Reply Ke Pesan Pengguna Yang Ingin Anda Scan.```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Reply Ke Pesan Pengguna Yang Ingin Di Scann.```")
+        await steal.edit("```Reply Ke Pesan Pengguna Yang Ingin Di Scan.```")
         return
-    await steal.edit("__C__")
-    await steal.edit("__Co__")
-    await steal.edit("__Con__")
-    await steal.edit("__Conn__")
-    await steal.edit("__Conne__")
-    await steal.edit("__Connec__")
-    await steal.edit("__Connect__")
-    await steal.edit("__Connecti__")
-    await steal.edit("__Connectin__")
-    await steal.edit("__Connecting__")
-    await steal.edit("__Connecting t__")
-    await steal.edit("__Connecting to__")
-    await steal.edit("__Connecting to s__")
-    await steal.edit("__Connecting to se__")
-    await steal.edit("__Connecting to ser__")
-    await steal.edit("__Connecting to serv__")
-    await steal.edit("__Connecting to serve__")
-    await steal.edit("__Connecting to server__")
-    await steal.edit("__Connecting to server.__")
-    await steal.edit("__Connecting to server..__")
-    await steal.edit("__Connecting to server...__")
+    await steal.edit("__Sedang Memprosess__")
+    await steal.edit("__Sedang Memprosess__")
+    await steal.edit("__Sedang Memprosess__")
+    await steal.edit("__Sedang Memprosess__")
     try:
         async with bot.conversation(chat) as conv:
             try:
@@ -67,7 +50,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Saya Tidak Menemukan Informasi Pergantian Nama Ini Yang Mulia, Orang Ini Belum Pernah Mengganti Nama Sebelumnya```")
+                await steal.edit("```Saya Tidak Menemukan Informasi Pergantian Nama Ini, Orang Ini Belum Pernah Mengganti Nama Sebelumnya```")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -79,7 +62,7 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await steal.edit("`Saya Sedang Sakit Yang Mulia, Mohon Maaf`")
+        return await steal.edit("`Saya Sedang Sakit, Mohon Maaf`")
 
 
 CMD_HELP.update({
