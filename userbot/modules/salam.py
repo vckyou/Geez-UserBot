19636c23ed8c370a05dfaef386056f13e61f789a
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-20 14:46:24 +0700

    okehhbaiklah

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -10,13 +10,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern='^.P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb. ...`")
+    await typew.edit("`Assalamu'alaikum wr. wb.`")
 
 
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb. ...`")
+    await typew.edit("`Assalamu'alaikum wr. wb.`")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
@@ -33,8 +33,8 @@ async def typewriter(typew):
 
 CMD_HELP.update({
     "salam":
-    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.P`\
+    "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.P` | `.p`\
 \n↳ : Untuk Memberi salam.\
-\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.L`\
+\n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.L` `.l`\
 \n↳ : Untuk Menjawab Salam."
 })
