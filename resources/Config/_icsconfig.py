60b69994ec86464727bc6a65cef0a573209e51fc
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-23 14:27:28 +0700

    Update _icsconfig.py

--- resources/Config/_icsconfig.py
+++ resources/Config/_icsconfig.py
@@ -2,5 +2,18 @@ import os
 from telethon.tl.types import ChatBannedRights
 
 
+
+class Config(object):
+    LOGGER = True
+
+
 AUTONAME = os.environ.get("AUTONAME", None)
 CHANGE_TIME = int(os.environ.get("CHANGE_TIME", 60))
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
