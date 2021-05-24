7f0af2aa009595cc903a5375a3249a0f26d882b1
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-24 16:55:05 +0700

    Update destination.py

--- userbot/modules/destination.py
+++ userbot/modules/destination.py
@@ -1,10 +1,8 @@
-# port by KOALA 🐨 /@manusiarakitann
-
 from userbot.events import register
 from userbot import CMD_HELP
 
-
-@register(outgoing=True, pattern=r"^\.(?:gsend|gchat)\s?(.*)?")
+#ported By @VckyouuBitch From GeezProject
+@register(outgoing=True, pattern=r"^\.(?:dm)\s?(.*)?")
 async def remoteaccess(event):
 
     p = event.pattern_match.group(1)
@@ -12,7 +10,7 @@ async def remoteaccess(event):
 
     chat_id = m[0]
     try:
-        chat_id = int(chat_id)
+        chat_id = int(chat_message)
     except BaseException:
 
         pass
@@ -21,19 +19,19 @@ async def remoteaccess(event):
     mssg = await event.get_reply_message()
     if event.reply_to_msg_id:
         await event.client.send_message(chat_id, mssg)
-        await event.edit("`Mengirim Pesan Ke Tujuan Anda.`")
+        await event.edit("`Success Mengirim Pesan Anda.`")
     for i in m[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
         await event.client.send_message(chat_id, msg)
-        await event.edit("Mengirim Pesan Ke Tujuan Anda.`")
+        await event.edit("Success Mengirim Pesan Anda.`")
     except BaseException:
-        await event.edit("** Gagal Mengirim Pesan, Emang Lu Join Grup Nya Njing ? **")
+        await event.edit("**Terjadi Error. Gagal Mengirim Pesan.**")
 
 CMD_HELP.update(
     {
-        "grouplink": "`.gsend` | `.gchat`\
-    \nMengirim Pesan Jarak Jauh Ke Grup Lain .gsend <link grup> <pesan>."
+        "message": "`.dm`\
+    \nMengirim Pesan Dengan Jarak Jauh Dengan .dm <username> <pesan>."
     })
