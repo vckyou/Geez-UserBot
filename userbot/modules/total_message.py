e60b7d2ddfdc5cb853e981ed4623b3faf9e76dc3
Geez <79180058+vckyou@users.noreply.github.com>
2021-05-24 17:50:49 +0700

    fixes bug

--- userbot/modules/total_message.py
+++ userbot/modules/total_message.py
@@ -8,17 +8,17 @@ from userbot import CMD_HELP, bot
 async def _(event):
     k = await event.get_reply_message()
     if k:
-        a = await event.client.get_messages(chat_id, 0, from_user=k.sender_id)
+        a = await bot.get_messages(chat_id, 0, from_user=k.sender_id)
         return await event.edit(f"Total msgs of {u} here = {a.total}")
     u = event.pattern_match.group(1)
     if not u:
         u = "me"
-    a = await event.client.get_messages(chat_id, 0, from_user=u)
+    a = await bot.get_messages(chat_id, 0, from_user=u)
     await event.edit(f'Total msgs of {u} here={a.total}")
 
 CMD_HELP.update(
     {
-        "totalmessage": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tmsg` | `.tmsg` <username>\
+        "totalmsg": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.tmsg` | `.tmsg` <username>\
     \n↳ : Returns your total msg count in current chat Or Returns total msg count of user in current chat."
     }
 )
