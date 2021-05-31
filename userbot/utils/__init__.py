33e8c8036b9f72c0ff41e71080d2665307d34538
vckyou <viicky.auliaz@gmail.com>
2021-05-31 02:29:17 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -18,4 +18,4 @@ from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress
-from .tools import humanbytes, time_formatter, human_to_bytes, md5,post_to_telegraph, take_screen_shot, runcmd
+from .tools import humanbytes, time_formatter, human_to_bytes, md5, post_to_telegraph, take_screen_shot, runcmd
