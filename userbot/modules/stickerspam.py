04923d64d5ffadfcae75f35be5392c3ef7d0205a
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-02 03:53:45 +0700

    Create stickerspam.py

---
+++ userbot/modules/stickerspam.py
@@ -0,0 +1,38 @@
+from telethon import utils
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID, InputStickerSetShortName
+
+from userbot.events import register
+from userbot import CMD_HELP
+
+
+@ultroid_cmd(pattern="sspam$")
+async def _(event):
+    x = await event.get_reply_message()
+    if not (x and x.media and hasattr(x.media, "document")):
+        return await event.edit("`Reply To Sticker Only`")
+    set = x.document.attributes[1]
+    sset = await bot(
+        GetStickerSetRequest(
+            InputStickerSetID(
+                id=set.stickerset.id,
+                access_hash=set.stickerset.access_hash,
+            )
+        )
+    )
+    pack = sset.set.short_name
+    docs = [
+        utils.get_input_document(x)
+        for x in (
+            await bot(GetStickerSetRequest(InputStickerSetShortName(pack)))
+        ).documents
+    ]
+    for xx in docs:
+        await event.respond(file=(xx))
+
+
+CMD_HELP.update(
+    {
+        "sspam": "`.sspam`\
+    \nUsage : Balas Ke Sticker."
+    })
