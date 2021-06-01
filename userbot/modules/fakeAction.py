aebbea3a91fe880b3a9eff2d3b8ce0505df87f85
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-02 03:26:36 +0700

    Fixes Bugs:)

--- userbot/modules/fakeAction.py
+++ userbot/modules/fakeAction.py
@@ -1,8 +1,8 @@
+# Port By @VckyouuBitch From Geez-Projects
+# # Copyright (C) 2021 Geez-Project
 from userbot.events import register
 from userbot import CMD_HELP
 import asyncio
-
-# Port By @VckyouuBitch From Geez-Projects
 
 
 @register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
@@ -18,8 +18,62 @@ async def _(event):
                 t = await event.ban_time(t)
             except BaseException:
                 return await event.edit("`Incorrect Format`")
-    await event.edit(f"`Starting Fake Typing For` {t} `sec.`")
+    await event.edit(f"`Starting Fake Typing For {t} sec.`")
     async with event.client.action(event.chat_id, "typing"):
+        await asyncio.sleep(t)
+
+
+@register(outgoing=True, pattern="^.faudio(?: |$)(.*)")
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("`Incorrect Format`")
+    await event.edit(f"`Starting Fake audio recording For {t} sec.`")
+    async with event.client.action(event.chat_id, "record-audio"):
+        await asyncio.sleep(t)
+
+
+@register(outgoing=True, pattern="^.fvideo(?: |$)(.*)")
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("`Incorrect Format`")
+    await event.edit(f"`Starting Fake video recording For {t} sec.`")
+    async with event.client.action(event.chat_id, "record-video"):
+        await asyncio.sleep(t)
+
+
+@register(outgoing=True, pattern="^.fgame(?: |$)(.*)")
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await event.ban_time(t)
+            except BaseException:
+                return await event.edit("`Incorrect Format`")
+    await event.edit(f"`Starting Fake Game Playing For {t} sec.`")
+    async with event.client.action(event.chat_id, "game"):
         await asyncio.sleep(t)
 
 CMD_HELP.update(
