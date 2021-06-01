b45aa0983c3b2f7600020742f4e46e53bd8903c7
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-02 00:32:20 +0700

    Create FakeAction.py

---
+++ userbot/modules/FakeAction.py
@@ -0,0 +1,27 @@
+from userbot.events import register
+from userbot import CMD_HELP, bot
+
+#Port By @VckyouuBitch From Geez-Projects
+@register(outgoing=True, pattern="^.ftyping(?: |$)(.*)")
+async def _(event):
+    t = event.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(t)
+            except BaseException:
+                return await event.edit("`Incorrect Format`")
+    await event.edit(f"Starting Fake Typing For {t} sec.")
+    async with event.client.action(event.chat_id, "typing"):
+        await asyncio.sleep(t)
+
+CMD_HELP.update(
+    {
+        "fakeaction": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.ftyping <jumlah text>`"
+        "\n• : Fake typing ini Berfungsi dalam group"
+    }
+)
