d31d80ff490b7d484e82253a9d4d2feaa917b594
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-03 14:32:11 +0700

    Dhlh
    Update fixes Nekobin

--- userbot/modules/allscrapers.py
+++ userbot/modules/allscrapers.py
@@ -811,51 +811,51 @@ async def neko(nekobin):
 
 
 @register(outgoing=True, pattern=r"^\.neko(?: |$)([\s\S]*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    datetime.now()
-    if not os.path.isdir(TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TMP_DOWNLOAD_DIRECTORY)
-    input_str = event.pattern_match.group(1)
-    message = "SYNTAX: `.neko <long text to include>`"
-    if input_str:
-        message = input_str
-    elif event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.media:
-            downloaded_file_name = await event.client.download_media(
-                previous_message,
-                TMP_DOWNLOAD_DIRECTORY,
-                progress_callback=progress,
+async def neko(nekobin):
+    """For .paste command, pastes the text directly to dogbin."""
+    nekobin_final_url = ""
+    match = nekobin.pattern_match.group(1).strip()
+    reply_id = nekobin.reply_to_msg_id
+
+    if not match and not reply_id:
+        return await pstl.edit("`Cannot paste text.`")
+
+    if match:
+        message = match
+    elif reply_id:
+        message = await nekobin.get_reply_message()
+        if message.media:
+            downloaded_file_name = await nekobin.client.download_media(
+                message,
+                TEMP_DOWNLOAD_DIRECTORY,
             )
             m_list = None
             with open(downloaded_file_name, "rb") as fd:
                 m_list = fd.readlines()
             message = ""
             for m in m_list:
-                # message += m.decode("UTF-8") + "\r\n"
                 message += m.decode("UTF-8")
             os.remove(downloaded_file_name)
         else:
-            message = previous_message.message
-    else:
-        message = "SYNTAX: `.neko <long text to include>`"
-    if downloaded_file_name.endswith(".py"):
-        py_file = ""
-        py_file += ".py"
-        data = message
-        key = (requests.post("https://nekobin.com/api/documents",
-                             json={"content": data}) .json() .get("result") .get("key"))
-        url = f"https://nekobin.com/{key}{py_file}"
-    else:
-        data = message
-        key = (requests.post("https://nekobin.com/api/documents",
-                             json={"content": data}) .json() .get("result") .get("key"))
-        url = f"https://nekobin.com/{key}"
+            message = message.text
 
-    reply_text = f"Pasted to Nekobin : [neko]({url})"
-    await event.edit_or_reply(reply_text)
+    # Nekobin
+    await nekobin.edit("`Pasting text . . .`")
+    resp = post(NEKOBIN_URL + "api/documents", json={"content": message})
+
+    if resp.status_code == 201:
+        response = resp.json()
+        key = response["result"]["key"]
+        nekobin_final_url = NEKOBIN_URL + key
+        reply_text = (
+            "`Pasted successfully!`\n\n"
+            f"[Nekobin URL]({nekobin_final_url})\n"
+            f"[View RAW]({NEKOBIN_URL}raw/{key})"
+        )
+    else:
+        reply_text = "`Gagal menjangkau Nekobin`"
+
+    await nekobin.edit(reply_text)
 
 
 @register(outgoing=True, pattern=r"^\.getpaste(?: |$)(.*)")
