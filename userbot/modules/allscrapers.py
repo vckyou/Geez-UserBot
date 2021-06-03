343082a08da61434839a1428426a23e6228d1413
vckyou <viicky.auliaz@gmail.com>
2021-06-03 06:39:13 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/allscrapers.py
+++ userbot/modules/allscrapers.py
@@ -845,21 +845,13 @@ async def _(event):
         py_file = ""
         py_file += ".py"
         data = message
-        key = (
-            requests.post("https://nekobin.com/api/documents", json={"content": data})
-            .json()
-            .get("result")
-            .get("key")
-        )
+        key = (requests.post("https://nekobin.com/api/documents",
+                             json={"content": data}) .json() .get("result") .get("key"))
         url = f"https://nekobin.com/{key}{py_file}"
     else:
         data = message
-        key = (
-            requests.post("https://nekobin.com/api/documents", json={"content": data})
-            .json()
-            .get("result")
-            .get("key")
-        )
+        key = (requests.post("https://nekobin.com/api/documents",
+                             json={"content": data}) .json() .get("result") .get("key"))
         url = f"https://nekobin.com/{key}"
 
     reply_text = f"Pasted to Nekobin : [neko]({url})"
