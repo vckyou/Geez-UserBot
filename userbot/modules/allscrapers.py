be2a8908d6b81623e2f27cb14cecd2412d734363
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-09 01:17:56 +0700

    Update allscrapers.py
    oke

--- userbot/modules/allscrapers.py
+++ userbot/modules/allscrapers.py
@@ -2,7 +2,6 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
 
 import asyncio
 import json
