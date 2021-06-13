75776195648e6bf2b056ee808b9b7dc98176e3e2
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-14 03:57:56 +0700

    yyyy

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -5,10 +5,15 @@
 #
 """Userbot module for keeping control who PM you."""
 
+import os
+import time
+import asyncio
+import io
 from sqlalchemy.exc import IntegrityError
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.functions.messages import ReportSpamRequest
 from telethon.tl.types import User
+from telethon import events, errors, functions, types
 
 from userbot import (
     BOTLOG,
@@ -19,19 +24,30 @@ from userbot import (
     LOGS,
     PM_AUTO_BAN,
     ALIVE_NAME,
+    CUSTOM_PMPERMIT_TEXT,
 )
 from userbot.events import register
+
+
+PM_PERMIT_PIC = os.environ.get("PM_PERMIT_PIC", None)
+if PM_PERMIT_PIC is None:
+  WARN_PIC = "https://telegra.ph/file/c86937b1224214421394a.jpg"
+else:
+  WARN_PIC = PM_PERMIT_PIC
+
+COUNT_PM = {}
+LASTMSG = {}
 
 # ========================= CONSTANTS ============================
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-
+CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT_TEXT) if CUSTOM_PMPERMIT_TEXT else f"│Karena Saya Akan Otomatis Memblokir\n│Anda, Tunggu Sampai {DEFAULTUSER}\n│Menerima Pesan Anda, Terimakasih.\n" 
 DEF_UNAPPROVED_MSG = (
     "◄┈─╼━━━━━━━━━━━━━━━━━╾─┈╮\n"
     "ㅤ“𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐓𝐡𝐞 𝐏𝐫𝐢𝐯𝐚𝐜𝐲 𝐌𝐞𝐬𝐬𝐚𝐠𝐞.”\n"
     "╭┈─╼━━━━━━━━━━━━━━━━━╾─┈╯\n"
     "│❗𝘿𝙄𝙇𝘼𝙍𝘼𝙉𝙂 𝙈𝙀𝙇𝘼𝙆𝙐𝙆𝘼𝙉 𝙎𝙋𝘼𝙈❗\n│\n"
-    f"│Karena Saya Akan Otomatis Memblokir\n│Anda, Tunggu Sampai {DEFAULTUSER}\n│Menerima Pesan Anda, Terimakasih.\n"
+    f"{CUSTOM_MIDDLE_PMP}\n"
     "╰┈────────────────────┈─➤\n"
     "▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▰▱\n"
     "┣[○› `AUTOMATIC MESSAGES`\n"
