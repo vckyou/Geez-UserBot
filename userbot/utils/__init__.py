4a3404b4e00873d49c4be9fe18391851de814558
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-13 23:06:06 +0700

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -17,4 +17,4 @@ from .format import parse_pre
 from .chrome import chrome, options
 from .google_images_download import googleimagesdownload
 from .progress import progress, CancelProcess
-from .tools import humanbytes, time_formatter, human_to_bytes, md5, post_to_telegraph, take_screen_shot, runcmd
+from .tools import humanbytes, time_formatter, human_to_bytes, run_cmd, md5, post_to_telegraph, take_screen_shot, runcmd
