e16c9d85768fed140034281da298852bbac60e1e
vckyou <viicky.auliaz@gmail.com>
2021-06-16 13:44:12 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -535,7 +535,6 @@ with bot:
                 reply_pop_up_alert = f"🚫!WARNING!🚫 Jangan Menggunakan Milik {DEFAULTUSER}."
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"helpme_close\((.+?)\)")
@@ -603,11 +602,9 @@ with bot:
 
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
         @tgbot.on(events.CallbackQuery(data=b'close'))
         async def close(event):
             await event.edit("Button closed!", buttons=Button.clear())
-
 
     except BaseException:
         LOGS.info(
