821d674c3220935cd1950399b1316f5a48ebd898
Geez <79180058+vckyou@users.noreply.github.com>
2021-06-18 06:04:48 +0700

    hu

--- userbot/modules/kang_pack.py
+++ userbot/modules/kang_pack.py
@@ -21,7 +21,7 @@ async def pack_kangish(event):
     if len(event.text) > 9:
         _packname = event.text.split(" ", maxsplit=1)[1]
     else:
-        _packname = f"Geez Stickers Kang By {_.sender_id}"
+        _packname = f"Geez Stickers Kang By {event.sender_id}"
     if _e and _e.media and _e.media.document.mime_type == "image/webp":
         _id = _e.media.document.attributes[1].stickerset.id
         _hash = _e.media.document.attributes[1].stickerset.access_hash
@@ -45,32 +45,32 @@ async def pack_kangish(event):
                 udB.set("PACKKANG", "{}")
             ok = eval(udB.get("PACKKANG"))
             try:
-                pack = ok[_.sender_id] + 1
+                pack = ok[event.sender_id] + 1
             except BaseException:
                 pack = 1
             try:
                 _r_e_s = await bot(
                     functions.stickers.CreateStickerSetRequest(
-                        user_id=_.sender_id,
+                        user_id=event.sender_id,
                         title=_packname,
                         short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
                         stickers=stiks,
                     )
                 )
-                ok.update({_.sender_id: pack})
+                ok.update({event.sender_id: pack})
                 udB.set("PACKKANG", str(ok))
             except PackShortNameOccupiedError:
                 time.sleep(1)
                 pack += 1
                 _r_e_s = await bot(
                     functions.stickers.CreateStickerSetRequest(
-                        user_id=_.sender_id,
+                        user_id=event.sender_id,
                         title=_packname,
                         short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
                         stickers=stiks,
                     )
                 )
-                ok.update({_.sender_id: pack})
+                ok.update({event.sender_id: pack})
                 udB.set("PACKKANG", str(ok))
             await event.edit(f"Pack Kanged Successfully.\nKanged Pack: [link](https://t.me/addstickers/{_r_e_s.set.short_name})",
                              )
