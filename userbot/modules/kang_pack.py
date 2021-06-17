b2b09246c38d401355eaf97d40d475794ee59d02
vckyou <viicky.auliaz@gmail.com>
2021-06-17 22:19:01 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/kang_pack.py
+++ userbot/modules/kang_pack.py
@@ -24,57 +24,58 @@ async def pack_kangish(event):
     else:
         _packname = f"Geez Stickers Kang By {_.sender_id}"
     if _e and _e.media and _e.media.document.mime_type == "image/webp":
-       _id = _e.media.document.attributes[1].stickerset.id
+        _id = _e.media.document.attributes[1].stickerset.id
         _hash = _e.media.document.attributes[1].stickerset.access_hash
-        _get_stiks = await ultroid_bot(
-            functions.messages.GetStickerSetRequest(
-                stickerset=types.InputStickerSetID(id=_id, access_hash=_hash)
-            )
-        )
-        stiks = []
-        for i in _get_stiks.documents:
-            x = utils.get_input_document(i)
-            stiks.append(
-                types.InputStickerSetItem(
-                    document=x,
-                    emoji=(i.attributes[1]).alt,
+         _get_stiks = await ultroid_bot(
+              functions.messages.GetStickerSetRequest(
+                   stickerset=types.InputStickerSetID(
+                       id=_id, access_hash=_hash)
+                   )
+              )
+          stiks = []
+           for i in _get_stiks.documents:
+                x = utils.get_input_document(i)
+                stiks.append(
+                    types.InputStickerSetItem(
+                        document=x,
+                        emoji=(i.attributes[1]).alt,
+                    )
                 )
-            )
-        try:
-            eval(udB.get("PACKKANG"))
-        except BaseException:
-            udB.set("PACKKANG", "{}")
-        ok = eval(udB.get("PACKKANG"))
-        try:
-            pack = ok[_.sender_id] + 1
-        except BaseException:
-            pack = 1
-        try:
-            _r_e_s = await asst(
-                functions.stickers.CreateStickerSetRequest(
-                    user_id=_.sender_id,
-                    title=_packname,
-                    short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
-                    stickers=stiks,
+            try:
+                eval(udB.get("PACKKANG"))
+            except BaseException:
+                udB.set("PACKKANG", "{}")
+            ok = eval(udB.get("PACKKANG"))
+            try:
+                pack = ok[_.sender_id] + 1
+            except BaseException:
+                pack = 1
+            try:
+                _r_e_s = await asst(
+                    functions.stickers.CreateStickerSetRequest(
+                        user_id=_.sender_id,
+                        title=_packname,
+                        short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
+                        stickers=stiks,
+                    )
                 )
-            )
-            ok.update({_.sender_id: pack})
-            udB.set("PACKKANG", str(ok))
-        except PackShortNameOccupiedError:
-            time.sleep(1)
-            pack += 1
-            _r_e_s = await asst(
-                functions.stickers.CreateStickerSetRequest(
-                    user_id=_.sender_id,
-                    title=_packname,
-                    short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
-                    stickers=stiks,
+                ok.update({_.sender_id: pack})
+                udB.set("PACKKANG", str(ok))
+            except PackShortNameOccupiedError:
+                time.sleep(1)
+                pack += 1
+                _r_e_s = await asst(
+                    functions.stickers.CreateStickerSetRequest(
+                        user_id=_.sender_id,
+                        title=_packname,
+                        short_name=f"geez_{_.sender_id}_{pack}_by_{(await tgbot.get_me()).username}",
+                        stickers=stiks,
+                    )
                 )
-            )
-            ok.update({_.sender_id: pack})
-            udB.set("PACKKANG", str(ok))
-        await event.edit(f"Pack Kanged Successfully.\nKanged Pack: [link](https://t.me/addstickers/{_r_e_s.set.short_name})",
-                         )
+                ok.update({_.sender_id: pack})
+                udB.set("PACKKANG", str(ok))
+            await event.edit(f"Pack Kanged Successfully.\nKanged Pack: [link](https://t.me/addstickers/{_r_e_s.set.short_name})",
+                             )
     else:
         await event.edit("Unsupported File")
 
