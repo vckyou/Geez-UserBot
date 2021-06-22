edff375b7a7f0ebaa864ef37e6cacf5c11979c8c
vckyou <viicky.auliaz@gmail.com>
2021-06-22 08:34:40 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/stickerspam.py
+++ userbot/modules/stickerspam.py
@@ -27,7 +27,7 @@ async def stickerpack_spam(event):
     try:
         stickerset_attr = reply.document.attributes[1]
         geezevent = await event.reply("`Fetching details of the sticker pack, please wait..`"
-                                     )
+                                      )
     except BaseException:
         await event.edit("`This is not a sticker. Reply to a sticker.`", 5)
         return
