1a8967ce1fd1dab9587f6e6eb148eeed33c87d84
vckyou <viicky.auliaz@gmail.com>
2021-06-22 01:58:26 +0000

    pylint: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -588,7 +588,6 @@ with bot:
                 reply_pop_up_alert = f"🚫!WARNING!🚫 Jangan Menggunakan Milik {DEFAULTUSER}."
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
         @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"ub_modul_(.*)")
