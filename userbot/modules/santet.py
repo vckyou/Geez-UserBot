a83733050140e9b2ccba7c0fd39361619a2550c3
executor99 <82030518+executor99@users.noreply.github.com>
2021-06-29 04:56:31 +0700

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,11 +6,11 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
+    await typew.edit("`Anda Telah Mengkordinir orang itu meninggal ツ`")
     sleep(2)
     await typew.edit("`Mencari Nama Orang Ini...`")
     sleep(1)
-    await typew.edit("`Santet Online Segera Dilakukan`")
+    await typew.edit("`Santet Si Anjing Sampe Mati`")
     sleep(1)
     await typew.edit("0%")
     number = 1
@@ -313,7 +313,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Mampus lu kontol bentar lagi mati`")
 
 
 CMD_HELP.update({
