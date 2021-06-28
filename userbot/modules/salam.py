f1b24c9725fb598754666d91570862975afef224
executor99 <82030518+executor99@users.noreply.github.com>
2021-06-29 04:54:45 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -16,13 +16,13 @@ async def typewriter(typew):
 @register(outgoing=True, pattern='^.p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb.`")
+    await typew.edit("`Assalamu'alaikum Sayang.`")
 
 
 @register(outgoing=True, pattern='^.L(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb.`")
+    await typew.edit("`Wa'alaikumssalam Ahli Surga.`")
 
 
 @register(outgoing=True, pattern='^.l(?: |$)(.*)')
