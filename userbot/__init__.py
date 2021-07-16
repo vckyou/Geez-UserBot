a821e7363ae95ff93140fcd28aa228e96544833c
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-16 21:25:26 +0700

    :'v

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -379,6 +379,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
     number_of_cols = 2
+    global lockpage
+    lockpage = page_number
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
