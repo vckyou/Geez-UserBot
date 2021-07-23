7a5fa37e0e0a638b11a2e1885132a453c79f9d84
vckyou <79180058+vckyou@users.noreply.github.com>
2021-07-23 14:43:07 +0700

    Okeh

--- userbot/modules/scraper.py
+++ userbot/modules/scraper.py
@@ -11,17 +11,90 @@
 import asyncio
 import csv
 import random
-
+from asyncio import sleep 
 from telethon.errors.rpcerrorlist import (
     UserAlreadyParticipantError,
     UserPrivacyRestrictedError,
     UserNotMutualContactError
 )
+from telethon.tl.functions.channels import (
+    InviteToChannelRequest,
+    EditBannedRequest,
+    GetFullChannelRequest)
+from telethon.tl.types import InputPeerUser, ChatBannedRights
+from telethon.tl import functions
+from telethon.tl.functions.messages import GetFullChatRequest
+from telethon.errors import (
+    ChannelInvalidError,
+    ChannelPrivateError,
+    ChannelPublicGroupNaError)
 
 from userbot.events import register
 from userbot import CMD_HELP
-from telethon.tl.functions.channels import InviteToChannelRequest
-from telethon.tl.types import InputPeerUser
+
+async def get_chatinfo(event):
+    chat = event.pattern_match.group(1)
+    chat_info = None
+    if chat:
+        try:
+            chat = int(chat)
+        except ValueError:
+            pass
+    if not chat:
+        if event.reply_to_msg_id:
+            replied_msg = await event.get_reply_message()
+            if replied_msg.fwd_from and replied_msg.fwd_from.channel_id is not None:
+                chat = replied_msg.fwd_from.channel_id
+        else:
+            chat = event.chat_id
+    try:
+        chat_info = await event.client(GetFullChatRequest(chat))
+    except BaseException:
+        try:
+            chat_info = await event.client(GetFullChannelRequest(chat))
+        except ChannelInvalidError:
+            await event.reply("`Invalid channel/group`")
+            return None
+        except ChannelPrivateError:
+            await event.reply("`This is a private channel/group or I am banned from there`")
+            return None
+        except ChannelPublicGroupNaError:
+            await event.reply("`Channel or supergroup doesn't exist`")
+            return None
+        except (TypeError, ValueError):
+            await event.reply("`Invalid channel/group`")
+            return None
+    return chat_info
+
+
+@register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
+async def get_users(event):
+    sender = await event.get_sender()
+    me = await event.client.get_me()
+    if not sender.id == me.id:
+        geez = await event.reply("`processing...`")
+    else:
+        geez = await event.edit("`processing...`")
+    geezteam = await get_chatinfo(event)
+    chat = await event.get_chat()
+    if event.is_private:
+        return await geez.edit("`Sorry, Can add users here`")
+    s = 0
+    f = 0
+    error = 'None'
+
+    await geez.edit("**TerminalStatus**\n\n`Collecting Users.......`")
+    async for user in event.client.iter_participants(geezteam.full_chat.id):
+        try:
+            if error.startswith("Too"):
+                return await geez.edit(f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people")
+            await event.client(functions.channels.InviteToChannelRequest(channel=chat, users=[user.id]))
+            s = s + 1
+            await geez.edit(f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`")
+        except Exception as e:
+            error = str(e)
+            f = f + 1
+    return await geez.edit(f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people")
 
 
 @register(outgoing=True, pattern=r"^\.getmemb$")
@@ -75,6 +148,45 @@ async def admem(event):
             n -= 1
             continue
 
+
+# Port By @VckyouuBitch From GeezProject
+# Perkontolan Dengan Hapus Credits
+@register(outgoing=True, pattern="^.allban(?: |$)(.*)")
+async def testing(event):
+    nikal = await event.get_chat()
+    chutiya = await event.client.get_me()
+    admin = nikal.admin_rights
+    creator = nikal.creator
+    if not admin and not creator:
+        await event.edit("Anda Tidak Mempunyai Hak")
+        return
+    await event.edit("Tidak Melakukan Apa-apa")
+# Thank for Dark_Cobra
+    everyone = await event.client.get_participants(event.chat_id)
+    for user in everyone:
+        if user.id == chutiya.id:
+            pass
+        try:
+            await event.client(EditBannedRequest(event.chat_id, int(user.id), ChatBannedRights(until_date=None, view_messages=True)))
+        except Exception as e:
+            await event.edit(str(e))
+        await sleep(.5)
+    await event.edit("Tidak Ada yang Terjadi di sini🙃🙂")
+
+
+CMD_HELP.update({
+    "inviteall":
+        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.inviteall groups username`\
+          \n📌 : __Scrapes users from the given chat to your group__."
+})
+
+CMD_HELP.update(
+    {
+        "allban": "**Plugin : **`allban`\
+    \n\n**Syntax : **`.allban`\
+    \n**Function : **ban all members in 1 cmnd"
+    }
+)
 
 CMD_HELP.update({
     "scraper":
