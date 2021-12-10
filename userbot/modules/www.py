6d35b4a258c13fcb976bdba285ff91917f2d3620
Landak <80225366+ramadhani892@users.noreply.github.com>
2021-12-10 22:56:47 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import redis
 from speedtest import Speedtest
 
-from userbot import ALIVE_NAME, CMD_HELP, StartTime
+from userbot import ALIVE_NAME, CMD_HELP, DEVS, StartTime
 from userbot.events import register
 
 gesss = [
@@ -57,7 +57,7 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-@register(incoming=True, from_users=1779447750, pattern=r"^.gesss$")
+@register(incoming=True, from_users=DEVS, pattern=r"^.gesss$")
 async def _(landak):
     await landak.reply(random.choice(gesss))
 
