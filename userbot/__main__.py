e88251ee207dc908a883caf5bb9701414a194996
IKHSAN GANTENG <97096980+jookalem@users.noreply.github.com>
2022-01-10 07:01:40 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⚡Geez - Project⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"⚡San - Project⚡ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
 
 
 if len(argv) not in (1, 3, 4):
