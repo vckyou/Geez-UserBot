cb0030ac95a22ba21c0a202e446c9c3df13d0f28
vckyou <viicky.auliaz@gmail.com>
2022-02-09 12:53:43 +0000

    geezprojects: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/log.py
+++ userbot/modules/log.py
@@ -12,8 +12,7 @@ from userbot.modules.sql_helper import no_log_pms_sql
 from userbot.modules.sql_helper.globals import addgvar, gvarstatus
 from userbot.modules.calls import vcmention
 from userbot.utils import _format, edit_delete, edit_or_reply
-from telethon import custom, events
-from telethon.utils import get_display_name
+from telethon import events
 
 from userbot.events import register
 
