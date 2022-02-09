a9ef770ff5d6a68e0af8b79c1098a0637cf0503b
vckyou <79180058+vckyou@users.noreply.github.com>
2022-02-10 02:49:40 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -243,8 +243,8 @@ async def edit_or_reply(
 eor = edit_or_reply
 
 async def apasih_pler():
-    geez = str(pybase64.b64decode("QEdlZXpQcm9qZWN0"))[2:18]
-    projects = str(pybase64.b64decode("QEdlZXpTdXBwb3J0"))[2:21]
+    geez = str(pybase64.b64decode("QEdlZXpQcm9qZWN0"))[2:13]
+    projects = str(pybase64.b64decode("QEdlZXpTdXBwb3J0"))[2:13]
     try:
         await bot(Get(geez))
     except BaseException:
