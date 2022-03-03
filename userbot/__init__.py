f756e555944c40e482920ac3966b42338c4081cf
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-03-04 06:12:04 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -413,7 +413,7 @@ else:
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
     number_of_cols = 2
-    global lockpage
+    global looters
     lockpage = page_number
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
