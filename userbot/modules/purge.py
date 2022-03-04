b817c70a69b8f1f5840859f1fb3437d67a003062
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-05 01:10:19 +0700

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# recode by @mrismanaziz
+# From GeezProjects <https://github.com/vckyou/Geez-Userbot>
+
 """ Modul Userbot untuk menghapus pesan yang tidak dibutuhkan (chat spam atau lainnya)."""
 
 
