0ff84d7f2d3817339f6af7b05793f8775fe23007
vckyou <viicky.auliaz@gmail.com>
2022-03-04 12:34:06 +0000

    geezprojects: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,7 +9,7 @@ import sys
 from importlib import import_module
 
 from telethon.tl.functions.channels import InviteToChannelRequest
-from userbot import ALIVE_NAME, BOT_USERNAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
+from userbot import ALIVE_NAME, BOTLOG_CHATID, BOT_USERNAME, BOT_VER, LOGS, bot
 from userbot.modules import ALL_MODULES
 from userbot.utils.events import checking
 
