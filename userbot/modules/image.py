999dbd168c920b7419aa32fb910c4ef7987dd57d
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-04 16:16:24 +0700

    fix images and improvements

--- userbot/modules/image.py
+++ userbot/modules/image.py
@@ -9,32 +9,35 @@ from userbot.utils import googleimagesdownload
 
 
 @register(outgoing=True, pattern="^.img (.*)")
-async def goimg(event):
+async def img_sampler(event):
+    """For .img command, search and return images matching the query."""
+    await event.edit("`Sedang Mencari Gambar Yang Anda Cari...`")
     query = event.pattern_match.group(1)
-    if not query:
-        return await event.edit("`Give something to search...`")
-    await event.edit("`Processing Keep Patience...`")
-    if ";" in query:
-        try:
-            lmt = int(query.split(";")[1])
-            query = query.split(";")[0]
-        except BaseExceptaion:
-            lmt = 5
-    else:
-        lmt = 5
-    gi = googleimagesdownload()
-    args = {
-        "keywords": query,
-        "limit": lmt,
-        "format": "jpg",
-        "output_directory": "./downloads/",
-    }
-    pth = gi.download(args)
-    ok = pth[0][query]
-    await event.client.send_file(event.chat_id, ok, caption=query, album=True)
-    rmtree(f"./downloads/{query}/")
-    await event.delete()
+    lim = findall(r"lim=\d+", query)
+    try:
+        lim = lim[0]
+        lim = lim.replace("lim=", "")
+        query = query.replace("lim=" + lim[0], "")
+    except IndexError:
+        lim = 15
+    response = googleimagesdownload()
 
+    # creating list of arguments
+    arguments = {
+        "keywords": query,
+        "limit": lim,
+        "format": "jpg",
+        "no_directory": "no_directory",
+    }
+
+    # passing the arguments to the function
+    paths = response.download(arguments)
+    lst = paths[0][query]
+    await event.client.send_file(
+        await event.client.get_input_entity(event.chat_id), lst
+    )
+    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
+    await event.delete()
 
 CMD_HELP.update(
     {
