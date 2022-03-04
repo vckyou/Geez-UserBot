c4f693812ade041331b0a935f42a2e70b92545a8
vckyou <viicky.auliaz@gmail.com>
2022-03-04 17:49:24 +0000

    geezprojects: auto fixes
    Signed-off-by: vckyou <viicky.auliaz@gmail.com>

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -14,7 +14,7 @@ import redis
 from speedtest import Speedtest
 
 from userbot import CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, humanbytes, geez_cmd
+from userbot.utils import edit_or_reply, geez_cmd
 from userbot import ALIVE_NAME, CMD_HELP, DEVS, StartTime
 from userbot.events import register
 
