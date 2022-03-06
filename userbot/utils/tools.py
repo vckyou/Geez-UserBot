4f793266d9316791ab25e1c46eda02eda0fc8cc3
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-06 19:23:58 +0700

    auto

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -279,11 +279,16 @@ eod = edit_delete
 async def checking():
     geez = str(pybase64.b64decode("R2VlelByb2plY3Q="))[2:13]
     projects = str(pybase64.b64decode("R2VlelN1cHBvcnQ="))[2:13]
+    ram = str(pybase64.b64decode("R2VlelByb2plY3Q="))[2:16]
     try:
         await bot(Get(geez))
     except BaseException:
         pass
     try:
         await bot(Get(projects))
+    except BaseException:
+        pass
+    try:
+        await bot(Get(ram))
     except BaseException:
         pass
