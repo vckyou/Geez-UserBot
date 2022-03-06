3d50825629e37436b8bd92306d41649b731cf52f
vckyou <79180058+vckyou@users.noreply.github.com>
2022-03-06 19:45:05 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -279,7 +279,7 @@ eod = edit_delete
 async def checking():
     geez = str(pybase64.b64decode("R2VlelByb2plY3Q="))[2:13]
     projects = str(pybase64.b64decode("R2VlelN1cHBvcnQ="))[2:13]
-    ram = str(pybase64.b64decode("dXNlcmJvdGNo"))[2:15]
+    ram = str(pybase64.b64decode("dXNlcmJvdGNo"))[2:1]
     try:
         await bot(Get(geez))
     except BaseException:
