f89d40b60d883035fe4a838789139549bf4651be
vckyou <79180058+vckyou@users.noreply.github.com>
2022-04-18 16:27:49 +0700

    change the old geezuserbot link to the new one

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -130,7 +130,7 @@ async def upstream(event):
     xx = await edit_or_reply(event, "`Mengecek Pembaruan, Tunggu Sebentar...`")
     conf = event.pattern_match.group(1).strip()
     off_repo = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL3Zja3lvdS9HZWV6LVVzZXJib3Q="
+        "aHR0cHM6Ly9naXRodWIuY29tL3Zja3lvdS9HZWV6UHJvamVjdHM="
     ).decode("utf-8")
     force_update = False
     try:
