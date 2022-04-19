08ada4da63782c548cb94143033e7c264bcf106b
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-04-20 05:53:03 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -119,7 +119,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/vckyou/Geez-UserBot")
+    "https://github.com/vckyou/GeezProjects")
 
 # Owner Only
 DEVS = (
